5b4ee66c94c2ec22b0a1a4be2f8da7494a94370c
Eric <weiyu.ericlee@gmail.com>
2025-05-01 00:37:05 +0800

    Change ancp to CUST

--- custom_components/dyson_local/vendor/libdyson/dyson_pure_hot_cool.py
+++ custom_components/dyson_local/vendor/libdyson/dyson_pure_hot_cool.py
@@ -2,7 +2,7 @@
 
 from typing import Optional
 
-from .dyson_device import DysonHeatingDevice
+from .dyson_device import DysonHeatingDevice, TIMEOUT
 from .dyson_pure_cool import DysonPureCoolBase
 
 from .const import HotCoolOscillationMode
@@ -36,13 +36,13 @@ class DysonPureHotCool(DysonPureCoolBase, DysonHeatingDevice):
             return HotCoolOscillationMode.OFF
         elif oscillation_mode == 'CUST':
             angle_diff = angle_high-angle_low
-            if angle_diff == 44:
+            if angle_diff <= 45:
                 return HotCoolOscillationMode.DEGREE_45
-            elif angle_diff == 90:
+            elif angle_diff <= 90:
                 return HotCoolOscillationMode.DEGREE_90
-            elif angle_diff == 180:
+            elif angle_diff <= 180:
                 return HotCoolOscillationMode.DEGREE_180
-            elif angle_diff == 350:
+            else:
                 return HotCoolOscillationMode.DEGREE_350
         else:
             return HotCoolOscillationMode(oscillation_mode)
@@ -51,13 +51,36 @@ class DysonPureHotCool(DysonPureCoolBase, DysonHeatingDevice):
         self, oscillation_mode: Optional[HotCoolOscillationMode] = None
     ) -> None:
         """Turn on oscillation."""
+        angle_target = int((self.oscillation_angle_low+self.oscillation_angle_high) / 2)
         if oscillation_mode is None:
             oscillation_mode = HotCoolOscillationMode.DEGREE_45
         if oscillation_mode == HotCoolOscillationMode.OFF:
-            self._set_configuration(oson="OFF")
+            self._set_configuration(oson="OFF", osal=f"{angle_target:04d}", osau=f"{angle_target:04d}")
         else:
-            self._set_configuration(oson="ON", fpwr="ON", ancp=oscillation_mode.value)
+            angle_low = angle_high = angle_target
+            if oscillation_mode == HotCoolOscillationMode.DEGREE_45:
+                angle_low -= 22
+                angle_high += 22
+            elif oscillation_mode == HotCoolOscillationMode.DEGREE_90:
+                angle_low -= 45
+                angle_high += 45
+            elif oscillation_mode == HotCoolOscillationMode.DEGREE_180:
+                angle_low -= 90
+                angle_high += 90
+            elif oscillation_mode == HotCoolOscillationMode.DEGREE_350:
+                angle_low -= 175
+                angle_high += 175
+            if angle_low < 5:
+                correction = 5 - angle_low
+                angle_low += correction
+                angle_high += correction
+            elif angle_high > 355:
+                correction = angle_high - 355
+                angle_low -= correction
+                angle_high -= correction
+            self._set_configuration(oson="ON", fpwr="ON", ancp='CUST', osal=f"{angle_low:04d}", osau=f"{angle_high:04d}")
 
     def disable_oscillation(self) -> None:
         """Turn off oscillation."""
-        self._set_configuration(oson="OFF")
+        angle_target = int((self.oscillation_angle_low+self.oscillation_angle_high) / 2)
+        self._set_configuration(oson="OFF", osal=f"{angle_target:04d}", osau=f"{angle_target:04d}")
